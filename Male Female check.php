0e129fee4e8aef4bc8886dc4d589a40bf2ca2513
zakir <worldking194@gmail.com>
2022-01-04 18:03:34 +0600

    1st commit

---
+++ Male Female check.php
@@ -0,0 +1,16 @@
+<?php
+$a="male";
+$b=10;
+if($a == "male"){
+    echo "Class 4";
+}else{
+    echo "Invalid Input";
+}
+echo "\n";
+$x="female";
+$y=10;
+if($x == "male" && $y==10){
+    echo "Class 4";
+}else{
+    echo "Invalid Input";
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Modulas:";
+echo 10%2;
+echo "\n";
+echo "Assaignment Oparetor: \n";
+$x=100;
+$y=100;
+$x++;
+--$y;
+echo $x;
+echo"\n";
+echo $y;
+echo "\n";
+$a=30;
+$b=40;
+$c=$a+$b;
+echo $c;
